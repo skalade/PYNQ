73d83e311d4f37c240037d3d8372028fcaafd215
skalade <sarunask@xilinx.com>
2022-06-23 07:22:24 -0700

    copies into /usr/lib instead of /lib/

--- sdbuild/packages/xrt/pre.sh
+++ sdbuild/packages/xrt/pre.sh
@@ -9,5 +9,5 @@ script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 sudo cp -f $script_dir/xrt_setup.sh $target/etc/profile.d
 cd $BUILD_ROOT/${PYNQ_BOARD}/petalinux_project
 petalinux-build -c zocl
-sudo rsync -av --keep-dirlinks $BUILD_ROOT/${PYNQ_BOARD}/petalinux_project/build/tmp/sysroots-components/*/zocl/usr $target
-sudo rsync -av --keep-dirlinks $BUILD_ROOT/${PYNQ_BOARD}/petalinux_project/build/tmp/sysroots-components/*/zocl/lib $target
+sudo cp -rf $BUILD_ROOT/${PYNQ_BOARD}/petalinux_project/build/tmp/sysroots-components/*/zocl/usr $target
+sudo cp -rf $BUILD_ROOT/${PYNQ_BOARD}/petalinux_project/build/tmp/sysroots-components/*/zocl/lib $target/usr/
